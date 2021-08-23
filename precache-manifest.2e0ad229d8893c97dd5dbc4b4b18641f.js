a3c20623e4c1d5c1c4033c85544dcd316c08870c
Darius Ilcevic <darekxlr@gmail.com>
2021-08-23 12:35:47 +0200

    Updates

---
+++ precache-manifest.2e0ad229d8893c97dd5dbc4b4b18641f.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9b50f75f1dae6d698d4d05835ffa3fd8",
+    "url": "/recipe-page/index.html"
+  },
+  {
+    "revision": "bf25af50b86402f401ef",
+    "url": "/recipe-page/static/css/main.96d247e4.chunk.css"
+  },
+  {
+    "revision": "8663436727948375c388",
+    "url": "/recipe-page/static/js/2.f003bf5c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/recipe-page/static/js/2.f003bf5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf25af50b86402f401ef",
+    "url": "/recipe-page/static/js/main.815179bd.chunk.js"
+  },
+  {
+    "revision": "efe8b049eb1806444c7b",
+    "url": "/recipe-page/static/js/runtime-main.bdfe1492.js"
+  },
+  {
+    "revision": "1076c095c4a196f01889e2bb0b0a3942",
+    "url": "/recipe-page/static/media/salad-2756467_1280.1076c095.jpg"
+  },
+  {
+    "revision": "9a8a97e3470e47112dfa8c628bdd4d7f",
+    "url": "/recipe-page/static/media/silesian-791329_1280.9a8a97e3.jpg"
+  },
+  {
+    "revision": "d3076466a8c2011f2194aac062e7841b",
+    "url": "/recipe-page/static/media/soup-1429793_1280.d3076466.jpg"
+  },
+  {
+    "revision": "2e158e05b02bc07dcb193111c1504d19",
+    "url": "/recipe-page/static/media/vegetables-2977891_1280.2e158e05.jpg"
+  }
+]);
